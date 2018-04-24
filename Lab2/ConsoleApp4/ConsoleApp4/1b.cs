116640ad9f00df937c6c358a2eb540a01f23d9a5
volodimirgoloshivskiy <32980294+volodimirgoloshivskiy@users.noreply.github.com>
2018-04-24 13:04:20 +0300

    Add files via upload

---
+++ Lab2/ConsoleApp4/ConsoleApp4/1b.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string BinaryHolder = "0";
+            Console.WriteLine("multiplicand");
+            Int16 x = Int16.Parse(Console.ReadLine());
+            string Multiplicand = ToBinary(x);
+            Console.WriteLine("multiplier");
+            Int16 y = Int16.Parse(Console.ReadLine());
+            string Multiplier = ToBinary(y);
+            string AbsoluteResult = "00000000000000000000000000000000";
+
+
+            for (int i = 0; i < 16; i++)
+            {
+                if (Multiplier.Substring(Multiplier.Length - i - 1, 1) == "0")
+                {
+                    AbsoluteResult = AbsoluteResult.Remove(AbsoluteResult.Length - 1, 1);
+                    AbsoluteResult = AbsoluteResult.Insert(0, "0");
+                }
+                else
+                {
+                    for (int j = 15; j >= 0; j--)
+                    {
+                        if (AbsoluteResult.Substring(j, 1) == "0")
+                        {
+                            AbsoluteResult = AbsoluteResult.Remove(j, 1);
+                            if (Multiplicand.Substring(j, 1) == "0")
+                            {
+                                if (BinaryHolder == "0")
+                                {
+                                    AbsoluteResult = AbsoluteResult.Insert(j, "0");
+                                }
+                                else
+                                {
+                                    AbsoluteResult = AbsoluteResult.Insert(j, "1");
+                                    BinaryHolder = "0";
+                                }
+                            }
+                            else
+                            {
+                                if (BinaryHolder == "0")
+                                {
+                                    AbsoluteResult = AbsoluteResult.Insert(j, "1");
+                                }
+                                else
+                                {
+                                    AbsoluteResult = AbsoluteResult.Insert(j, "0");
+                                    BinaryHolder = "1";
+                                }
+                            }
+                        }
+                        else
+                        {
+                            AbsoluteResult = AbsoluteResult.Remove(j, 1);
+                            if (Multiplicand.Substring(j, 1) == "0")
+                            {
+                                if (BinaryHolder == "0")
+                                {
+                                    AbsoluteResult = AbsoluteResult.Insert(j, "1");
+                                }
+                                else
+                                {
+                                    AbsoluteResult = AbsoluteResult.Insert(j, "0");
+                                    BinaryHolder = "1";
+                                }
+                            }
+                            else
+                            {
+                                if (BinaryHolder == "0")
+                                {
+                                    AbsoluteResult = AbsoluteResult.Insert(j, "0");
+                                    BinaryHolder = "1";
+                                }
+                                else
+                                {
+                                    AbsoluteResult = AbsoluteResult.Insert(j, "1");
+                                    BinaryHolder = "1";
+                                }
+                            }
+                        }
+                    }
+                    AbsoluteResult = AbsoluteResult = AbsoluteResult.Remove(AbsoluteResult.Length - 1, 1);
+                    AbsoluteResult = AbsoluteResult.Insert(0, "0");
+                }
+            }
+            Console.WriteLine("результат в двійковій формі" + "\n" + AbsoluteResult + "\n" + "правильний результат=" + (x * y) + "\n" + "переведений результат=" + BinToInt(AbsoluteResult));
+            Console.ReadLine();
+
+        }
+        static int BinToInt(string binaryNumber)
+        {
+            int multiplier = 1;
+            int converted = 0;
+            for (int i = binaryNumber.Length - 1; i >= 0; i--)
+            {
+                int t = System.Convert.ToInt16(binaryNumber[i].ToString());
+                converted = converted + (t * multiplier);
+                multiplier = multiplier * 2;
+            }
+            return converted;
+        }
+        static string ToBinary(int Decimal)
+        {
+            int BinaryHolder;
+            string BinaryResult = "";
+            while (Decimal > 0)
+            {
+                BinaryHolder = Decimal % 2;
+                BinaryResult += BinaryHolder;
+                Decimal = Decimal / 2;
+            }
+            char [] BinaryArray = BinaryResult.ToCharArray();
+            Array.Reverse(BinaryArray);
+            BinaryResult = new string(BinaryArray);
+            do
+            {
+                BinaryResult = BinaryResult.Insert(0, "0");
+            }
+            while (BinaryResult.Length < 16);
+            Console.WriteLine(BinaryResult);
+            return BinaryResult;
+        }
+    }
+}
---
+++ Lab2/ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dividend ");
+            Int32 dividend = Int32.Parse(Console.ReadLine());
+            string bsdividend = ToBinary(dividend);
+            Console.WriteLine("divisor ");
+            Int32 divisor = Int32.Parse(Console.ReadLine());
+            string bsdivisor = ToBinary(divisor);
+            string quotient = "";
+            if (dividend < divisor)
+            {
+                Console.WriteLine("Quotient=0     Remainder=" + dividend);
+            }
+            else
+            {
+                int i = 0;
+                string c = bsdividend;
+                do
+                {
+                    try
+                    {
+                        c = bsdividend.Remove(i);
+                    }
+                    catch (Exception e) { c = bsdividend; }
+                    if (BinToInt(c) < divisor)
+                    {
+                        quotient = quotient + "0";
+                    }
+                    else
+                    {
+                        quotient = quotient + "1";
+                        bsdividend = ToBinary(BinToInt(bsdividend) - (BinToInt(bsdivisor) << 16 - i));
+                    }
+                    i++;
+                }
+                while (i <= 16);
+                Console.WriteLine("Quotient = " + BinToInt(quotient) + "Remainder" + BinToInt(bsdividend));
+            }
+            Console.ReadKey();
+        }
+
+        static int BinToInt(string binaryNumber)
+        {
+            int multiplier = 1;
+            int converted = 0;
+            for (int i = binaryNumber.Length - 1; i >= 0; i--)
+            {
+                int t = System.Convert.ToInt16(binaryNumber[i].ToString());
+                converted = converted + (t * multiplier);
+                multiplier = multiplier * 2;
+            }
+            return converted;
+        }
+
+        static string ToBinary(int Decimal)
+        {
+            int BinaryHolder;
+            string BinaryResult = "";
+            while (Decimal > 0)
+            {
+                BinaryHolder = Decimal % 2;
+                BinaryResult += BinaryHolder;
+                Decimal = Decimal / 2;
+            }
+            char[] BinaryArray = BinaryResult.ToCharArray();
+            Array.Reverse(BinaryArray);
+            BinaryResult = new string(BinaryArray);
+            do
+            {
+                BinaryResult = BinaryResult.Insert(0, "0");
+            }
+            while (BinaryResult.Length < 16);
+            return BinaryResult;
+        }
+    }
+}
